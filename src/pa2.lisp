b149fae3aa7179562819f6f10280a71429bab85e
mattrichard <matthew.w.richard@gmail.com>
2013-04-15 21:45:32 -0600

    Canoe now holds up to two people.

--- src/pa2.lisp
+++ src/pa2.lisp
@@ -83,46 +83,78 @@
 ;;;; Bugs: None
 
 
-(defvar *state*)
-
-
 ;;; Constants used to indicate which side of the river the canoe is on
-(defconstant +canoe-left+ 1
-             "Value of the canoe when it is on left bank.")
-(defconstant +canoe-right+ -1
-             "Value of the canoe when it is on the right bank.")
 (defconstant +left-bank+ :left)
 (defconstant +right-bank+ :right)
+(defconstant +max-canoe-count+ 2 "Maximum number of missionaries or cannibals
+that the canoe can hold.")
 
-
-(defun get-canoe-bank (state)
-  "Maps the canoe's current state into the proper bank constant
-(i.e. +left-bank+ or +right-bank+, or nil if unknown)."
-  (cond
-    ((is-canoe-left state) +left-bank+)
-    ((is-canoe-right state) +right-bank+)
-    (t nil)))
 
 (defun get-other-bank (bank)
-  "Returns the bank opposite to the given bank,
-or nil if the given bank is unknown."
+  "Returns the bank opposite to the given bank, or nil if the given bank is 
+unknown."
   (cond
-    ((= bank +left-bank+) +right-bank+)
-    ((= bank +right-bank+) +left-bank+)
+    ((eql bank +left-bank+) +right-bank+)
+    ((eql bank +right-bank+) +left-bank+)
     (t nil)))
 
 
+(defmacro get-bank (state bank)
+  "Macro for getting the given bank from the state plist."
+  `(getf ,state ,bank))
+
 (defmacro get-m (state bank)
-  "Returns the number of missionaries on the given bank."
-  `(getf (getf ,state ,bank) :missionaries))
+  "Macro for getting the number of missionaries on the given bank."
+  `(getf (get-bank ,state ,bank) :missionaries))
 
 (defmacro get-c (state bank)
-  "Returns the number of cannibals on the given bank."
-  `(getf (getf ,state ,bank) :cannibals))
+  "Macro for getting the number of cannibals on the given bank."
+  `(getf (get-bank ,state ,bank) :cannibals))
 
 (defmacro get-canoe (state)
-  "Returns the state of the canoe."
+  "Macro for getting the state of the canoe."
   `(getf ,state :canoe))
+
+(defmacro get-canoe-bank (state)
+  "Macro for getting the current bank the canoe is at."
+  `(getf (get-canoe ,state) :bank))
+
+(defmacro get-canoe-seats (state)
+  "Macro for getting the seats of the canoe."
+   `(getf (get-canoe ,state) :seats))
+
+
+(defun get-canoe-count (state)
+  "Returns the number of missionaries and cannibals currently in the canoe."
+  (length (get-canoe-seats state)))
+
+(defun is-canoe-empty (state)
+  "Returns t if there are no missionaries nor cannibals in the canoe; 
+otherwise, returns nil."
+  (zerop (get-canoe-count state)))
+
+(defun is-canoe-full (state)
+  "Returns t if there are 2 people (missionaries or cannibals on the canoe; 
+otherwise, returns nil."
+  (= (get-canoe-count state) +max-canoe-count+))
+
+(defun is-canoe-left (state)
+  "Returns t if the canoe is on the left bank; otherwise, returns nil."
+  (eql (get-canoe-bank state) +left-bank+))
+
+(defun is-canoe-right (state)
+  "Returns t if the canoe is on the right bank; otherwise, returns nil."
+  (eql (get-canoe-bank state) +right-bank+))
+
+;(defun canoe-state-to-string (state)
+;  (cond
+;    ((is-canoe-left state) "left")
+;    ((is-canoe-right state) "right")
+;    (t "unknown")))
+
+(defun load-canoe (person &optional (num 1)))
+
+(defun unload-canoe (person &optional (num 1)))
 
 
 (defun dec-m (state bank)
@@ -143,8 +175,10 @@ or nil if the given bank is unknown."
 
 
 (defun move-canoe (state)
-  "Moves the canoe from the current bank to the other"
-  (setf (get-canoe state) (- (get-canoe state))))
+  "Moves the canoe from the current bank to the other."
+  (setf (get-canoe-bank state) (get-other-bank (get-canoe-bank state))))
+;  "Moves the canoe from the current bank to the other"
+;  (setf (get-canoe state) (- (get-canoe state))))
 
 (defun move-one-m (state)
   "Moves one missionary from the current bank to the other."
@@ -199,21 +233,6 @@ or nil if the given bank is unknown."
       (move-canoe state))))
 
 
-(defun is-canoe-left (state)
-  "Returns t if the canoe is on the left bank; otherwise, returns nil."
-  (= (getf state :canoe) +canoe-left+))
-
-(defun is-canoe-right (state)
-  "Returns t if the canoe is on the right bank; otherwise, returns nil."
-  (= (getf state :canoe) +canoe-right+))
-
-(defun canoe-state-to-string (state)
-  (cond
-    ((is-canoe-left state) "left")
-    ((is-canoe-right state) "right")
-    (t "unknown")))
-
-
 (defun print-header ()
   (format t "left side~15tright side~30tcanoe~40tlast move~%")
   (format t "---------~15t----------~30t-----~40t---------~%"))
@@ -224,36 +243,30 @@ or nil if the given bank is unknown."
             (get-c state +left-bank+)
             (get-m state +right-bank+)
             (get-c state +right-bank+)
-            (canoe-state-to-string state)))
+            (get-canoe-bank state)))
 
 
-(defmacro create-m-c-plist (m c)
-  "Generates a plist with properties 'missionaries' and 'cannibals' with the 
-values of m and c, respectively."
-  `(list :missionaries ,m :cannibals ,c))
+(defun create-m-c-plist (&key (m 0) (c 0))
+  "Returns a plist containing the keys MISSIONARIES and CANNIBALS and values 
+:m and :c, respectively."
+  (list :missionaries m :cannibals c))
 
-(defmacro create-bank-plist (bank-name m c)
-  "Generates a plist with the value of bank being the property whose value is a 
-plist created from create-m-c-plist."
-  `(list ,bank-name (create-m-c-plist ,m ,c)))
+(defun create-bank-plist (bank-key &key (m 0) (c 0))
+  "Returns a plist with the key as bank-key and the value a plist created from 
+create-m-c-plist."
+  (list bank-key (create-m-c-plist :m m :c c)))
 
-(defmacro create-state-plist (m c)
+(defun create-canoe-plist (&optional (canoe-bank +left-bank+))
+  (list :canoe (list :bank canoe-bank :seats ())))
+
+(defun create-state-plist (m c)
   "Generates a plist used as the data structure for solving the missionaries 
 and cannibals problem. The state is initialized as:
 (:LEFT (:MISSIONAIRES m :CANNIBALS c) :RIGHT (:MISSIONARIES 0 :CANNIBALS 0)
-:CANOE +canoe-left+)."
-;(:LEFT (:MISSIONAIRES m
-;        :CANNIBALS c)
-; :RIGHT (:MISSIONARIES 0
-;         :CANNIBALS 0)
-; :CANOE (:BANK +canoe-left+
-;         :SEAT-1 (:USED nil
-;                  :TYPE nil)
-;         :SEAT-2 (:USED nil
-;                  :TYPE nil)))
-  `(append (create-bank-plist +left-bank+ ,m ,c)
-           (create-bank-plist +right-bank+ 0 0)
-           (list :canoe +canoe-left+)))
+:CANOE (:BANK +left-bank+ :SEATS ())."
+  (append (create-bank-plist +left-bank+ :m m :c c)
+          (create-bank-plist +right-bank+)
+          (create-canoe-plist)))
 
 
 (defun success (state)
@@ -263,26 +276,34 @@ and cannibals problem. The state is initialized as:
        (= (get-c state +left-bank+) 0)
        (is-canoe-right state)))
 
+(defun generate-next-states (state)
+  ;TODO: write this
+  )
 
-(defun dfs (state success-form)
+(defun dfs (state success-form generator-form)
   ;; Check for success
   (if (funcall success-form state)
       (return t))
-
-  ;(let ((next-state state))
-;        (current-bank (get-canoe-bank state)))
-    ;(when (is-canoe-left state)
-      ;(do ()
-          ;((> get-m state +left-bank+))
-
+  (dolist (next-state (funcall generator-form state))
+    (if (dfs next-state #'success-form #'generator-form)
+        (return t)))
   nil)
 
 
 (defun m-c (m c)
   (let ((state (create-state-plist m c)))
     (print-header)
-    (print-state state)
-    (dfs state #'success)))
+    ;(print state)))
+    (print-state state)))
+    ;(inc-m state +left-bank+)
+    ;(print-state state)))
+    ;(inc-c state +left-bank+)
+    ;(print-state state)
+    ;(inc-m state +right-bank+)
+    ;(print-state state)
+    ;(inc-c state +right-bank+)
+    ;(print-state state)))
+    ;(dfs state #'success #'generate-next-states)))
 
 
 (m-c -1 -1)
